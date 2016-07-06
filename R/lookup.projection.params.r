c66954f38cd3899650601d022112e88be0dcab99
amcook <amcook127@gmail.com>
2016-07-06 13:03:29 -0300

    adding to lookup.projection.params.r

--- R/lookup.projection.params.r
+++ R/lookup.projection.params.r
@@ -10,6 +10,7 @@ lookup.projection.params = function(x) {
       lambert.conic             = "+proj=lcc +ellps=WGS84  +lon_0=63W +lat_0=45N +lat_1=43N +lat_2=47N +units=km ",
       lambert.conic.4t          = "+proj=lcc +ellps=WGS84  +lon_0=63W +lat_0=47N +lat_1=45N +lat_2=49N +units=km ",
       lambert.conic.canada.east = "+proj=lcc +ellps=WGS84  +lon_0=62W +lat_0=45N +lat_1=43N +lat_2=47N +units=km ",
+      lambert.conic.us.4x      = "+proj=lcc +ellips=WGS84  +lon_0=68W +lat_0=42N +lat_1=39N +lat_2=46N +units=km ",
       tmercator         = "+proj=tmerc +ellps=WGS84 +x_0=500000 +y=-400000 +lon_0=90w +units=km "
     )
     return ( out )
