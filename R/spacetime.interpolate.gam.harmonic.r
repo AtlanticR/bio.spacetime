1bf88c61795a8c875370037cd82d58e021775222
Jae S Choi <choijaeseok@gmail.com>
2016-09-12 12:33:12 -0300

    typo

--- R/spacetime.interpolate.gam.harmonic.r
+++ R/spacetime.interpolate.gam.harmonic.r
@@ -1,7 +1,7 @@
 
 
 spacetime.interpolate.gam.harmonic = function( ip, p ) {
-  #\\ harmonic in time method 
+  #\\ harmonic in time method
 
   if (exists( "libs", p)) RLibrary( p$libs )
   if (is.null(ip)) if( exists( "nruns", p ) ) ip = 1:p$nruns
@@ -11,49 +11,50 @@ spacetime.interpolate.gam.harmonic = function( ip, p ) {
 
 
 if(0) {
+
   mf = switch( p$tsmethod ,
     annual = ' t ~ s(yr) ',
-    seasonal.basic = ' t ~ s(yr) + s(dyear, bs="cc") ', 
-    seasonal.smoothed = ' t ~ s(yr, dyear) + s(yr) + s(dyear, bs="cc")  ', 
-    harmonics.1 = ' t ~ s(yr) + s(yr, cos.w) + s(yr, sin.w) + s(cos.w) + s(sin.w)  ', 
-    harmonics.2 = ' t ~ s(yr) + s(yr, cos.w) + s(yr, sin.w) + s(cos.w) + s(sin.w) + s(yr, cos.w2) + s(yr, sin.w2) + s(cos.w2) + s( sin.w2 ) ' , 
+    seasonal.basic = ' t ~ s(yr) + s(dyear, bs="cc") ',
+    seasonal.smoothed = ' t ~ s(yr, dyear) + s(yr) + s(dyear, bs="cc")  ',
+    harmonics.1 = ' t ~ s(yr) + s(yr, cos.w) + s(yr, sin.w) + s(cos.w) + s(sin.w)  ',
+    harmonics.2 = ' t ~ s(yr) + s(yr, cos.w) + s(yr, sin.w) + s(cos.w) + s(sin.w) + s(yr, cos.w2) + s(yr, sin.w2) + s(cos.w2) + s( sin.w2 ) ' ,
     harmonics.3 = ' t ~ s(yr) + s(yr, cos.w) + s(yr, sin.w) + s(cos.w) + s(sin.w) + s(yr, cos.w2) + s(yr, sin.w2) + s(cos.w2) + s( sin.w2 ) + s(yr, cos.w3) + s(yr, sin.w3)  + s(cos.w3) + s( sin.w3 ) '
   )
 
   mf = formula(mf)
-  
-  for ( dm in p$dist.km ) { 
+
+  for ( dm in p$dist.km ) {
       drange = c(-1,1) * dm
       plon0 = pp$plon + drange
       plat0 = pp$plat + drange
       i = which( bb$plon > plon0[1] & bb$plon < plon0[2] & bb$plat > plat0[1] & bb$plat < plat0[2] )
-      if (length(i) > p$nMin.tbot ) {  
-        
+      if (length(i) > p$nMin.tbot ) {
+
       #  browser()
 
         # only attempt interpolation if we have enough data (nMin.tbot)
         x = bb[i,] # faster to reduce the size of bb here
-        # remove potentially noisy/erroneous data --- they are highly influential when there is little data 
+        # remove potentially noisy/erroneous data --- they are highly influential when there is little data
         # xt = quantile( x$t, probs=c(0.005, 0.995) )
-        # xi = which( x$t >= xt[1] & x$t <= xt[2] ) 
+        # xi = which( x$t >= xt[1] & x$t <= xt[2] )
         # if (length(xi) < p$nMin.tbot ) next()
-        # x = x[xi, ] 
-        
+        # x = x[xi, ]
+
         x$w = 1 / (( pp$plon - x$plon)**2 + (pp$plat - x$plat)**2 )# weight data in space: inverse distance squared
         x$w[ which( x$w < 1e-3 ) ] = 1e-3
         x$w[ which( x$w > 1 ) ] = 1
 
-        # data transformations and creation of new variables where required for raw data 
+        # data transformations and creation of new variables where required for raw data
         if ( p$tsmethod %in% c( "harmonics.1", "harmonics.2", "harmonics.3"  ) ) {
           x$cos.w  = cos( 2*pi*x$tiyr )
           x$sin.w  = sin( 2*pi*x$tiyr )
-         
+
           years.with.data = unique( x$yr)
           no.years = which( !( zz$yr %in% years.with.data) )
-          zz$yr[ no.years ] = median( years.with.data) 
+          zz$yr[ no.years ] = median( years.with.data)
           zz$cos.w  = cos( zz$tiyr )
           zz$sin.w  = sin( zz$tiyr )
-          
+
           # compute additional harmonics only if required (to try to speed things up a bit)
           if ( p$tsmethod %in% c( "harmonics.2", "harmonics.3"  ) ) {
             x$cos.w2 = cos( 2*x$tiyr )
@@ -69,18 +70,18 @@ if(0) {
           }
         }
 
-  tsmodel = NULL 
-  tsmodel = try( gam( mf, data=x, weights=w, optimizer=c("outer","bfgs")  ) ) 
-  if ( ! "try-error" %in% class(tsmodel) ) {
-    out = try( predict( tsmodel, newdata=zz, type="response", se.fit=T ) ) 
-    if ( ! "try-error" %in% class( out ) ) {
-      zz$fit = out$fit
-      zz$se = out$se.fit 
-      break()  # candidate predictions found exit inner loop (dm)
-    }
+        tsmodel = NULL
+        tsmodel = try( gam( mf, data=x, weights=w, optimizer=c("outer","bfgs")  ) )
+        if ( ! "try-error" %in% class(tsmodel) ) {
+          out = try( predict( tsmodel, newdata=zz, type="response", se.fit=T ) )
+          if ( ! "try-error" %in% class( out ) ) {
+            zz$fit = out$fit
+            zz$se = out$se.fit
+            break()  # candidate predictions found exit inner loop (dm)
+          }
+        }
+      } 
   }
-
-
 }
 
 
@@ -225,12 +226,12 @@ if(0) {
         preds = NULL
 
       #  preds = .... model it here
-         
+
         if (exists("spacetime.invlink", p)) {
           preds$xmean =  p$spacetime.invlink( preds$xmean )
           preds$xsd =  p$spacetime.invlink( preds$xsd )
         }
-       
+
         if (is.null(preds)) {
           if ( debugrun) cat( paste( Sys.time(), deid, "prediction failure \n" ), file=p$debug.file, append=TRUE )
           next()
@@ -280,7 +281,7 @@ if(0) {
           P[ fi, means ] = pa$xmean[f]
           P[ fi, stdevs ] = pa$xsd[f]
         }
-      }
+      
       rm(MESH); gc()
     }
 
@@ -306,9 +307,10 @@ if(0) {
       x11(); levelplot( ( P[zz,means] ) ~ plon + plat, pps[zz,], aspect="iso", labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
     }
 
-    rm( ii, good, pa, xs, xm, mi, mf, si, sf ); gc()
-    if ( debugrun) cat( paste( Sys.time(), deid, "end \n" ), file=p$debug.file, append=TRUE )
+    rm( ii, good, pa, xs, xm, mi, mf, si, sf ) ; gc()
+    if ( debugrun) cat( paste( Sys.time(), deid, "end \n" ), file=p$debug.file, append=TRUE ) 
   }  # end for loop
+
   return( "complete" )
 
 }
