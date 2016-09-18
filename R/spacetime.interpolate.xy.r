0c9b2c7986ac934ad5fc1497f13abff51e90930e
Jae S Choi <choijaeseok@gmail.com>
2016-09-18 08:35:09 -0300

    add methods

---
+++ R/spacetime.interpolate.xy.r
@@ -0,0 +1,167 @@
+
+
+spacetime.interpolate.xy = function( interp.method, data, locsout, nr, nc, 
+  trimquants=TRUE, trimprobs=c(0.025, 0.975), theta=NULL, xwidth=theta*10, ywidth=theta*10 ) {
+  #\\ reshape statistics to fit the output resolution 
+  
+  # trim quaniles in case of extreme values
+  if (trimquants) {
+    vq = quantile( data$z, probs=trimprobs, na.rm=TRUE )
+    ii = which( data$z < vq[1] )
+    if ( length(ii)>0) data$z[ii] = vq[1]
+    jj = which( data$z > vq[2] )
+    if ( length(jj)>0) data$z[jj] = vq[2]
+  }
+  
+  out = res = NULL
+
+  if (interp.method == "kernel.density") {
+    # default :: create a "surface" and reshape to a grid using (gaussian) kernel-based smooth via FFT
+    require(fields)
+    isurf = fields::interp.surface( data, loc=locsout  )
+    zout = matrix( isurf, nrow=nr, ncol=nc )
+    out = fields::image.smooth( zout, theta=theta, xwidth=xwidth, ywidth=ywidth ) 
+    return (out$z)
+  }
+
+  # ------
+
+  if (interp.method == "inla.spde" ) {
+    require(INLA)
+
+    FM = "Y ~ -1 + intercept + f( spatial.field, model=SPDE )"
+    Y=data$z
+    locs= cbind(data$x, data$y)
+    plocs=locsout
+
+    method="fast"  # "direct" is slower and more accurate
+    nsamples=5000 
+
+    # identity links by default .. add more if needed here
+    spacetime.link = function(X) {X}
+    spacetime.invlink = function(X) {X}
+
+    if (link=="log" ) {
+      spacetime.link = function(X) {log(X)}
+      spacetime.invlink = function(X) {exp(X)}
+    }
+
+    locs = as.matrix( locs)
+
+    lengthscale = max( diff(range( locs[,1])), diff(range( locs[,2]) )) / 10  # in absence of range estimate take 1/10 of domain size
+
+    ndata = length(Y)
+    noise = lengthscale * 1e-9
+    locs = locs + runif( ndata*2, min=-noise, max=noise ) # add  noise  to prevent a race condition .. inla does not like uniform grids
+
+    MESH = spacetime.mesh( locs, lengthscale=lengthscale )
+    if ( is.null( MESH) ) return( "Mesh Error" )
+
+    SPDE = inla.spde2.matern( MESH,  alpha=2 ) # alpha is 2*nu (Bessel smoothness factor)
+
+    # FM = formula( Y ~ -1 + intercept + f( spatial.field, model=SPDE ) )
+    FM = formula( FM )
+    varY = as.character( FM[2] )
+    obs_index = inla.spde.make.index(name="spatial.field", SPDE$n.spde)
+    obs_eff = list()
+    obs_eff[["spde"]] = c( obs_index, list(intercept=1) )
+    obs_A = list( inla.spde.make.A( mesh=MESH, loc=locs[,] ) ) # no effects
+    obs_ydata = list()
+    obs_ydata[[ varY ]] = spacetime.link ( Y )
+    DATA = inla.stack( tag="obs", data=obs_ydata, A=obs_A, effects=obs_eff, remove.unused=FALSE )
+
+    if ( method=="direct") {
+      # direct method
+      preds_index = inla.spde.make.index( name="spatial.field", SPDE$n.spde)
+      preds_eff = list()
+      preds_eff[["spde"]] = c( list( intercept=rep(1,MESH$n )),
+           inla.spde.make.index(name="spatial.field", n.spde=SPDE$n.spde) )
+      ydata = list()
+      ydata[[ varY ]] = NA
+      Apreds = inla.spde.make.A(MESH, loc=as.matrix( plocs ) )
+      PREDS = inla.stack( tag="preds",
+        data=list( Y=NA),
+        A=list(Apreds),
+        effects=list(
+          c( list(intercept=rep(1, MESH$n)),
+             inla.spde.make.index( name="spatial.field", MESH$n)))
+      )
+      DATA = inla.stack(DATA, PREDS)
+      i_data = inla.stack.index( DATA, "preds")$data
+    }
+
+
+    RES = NULL
+    RES = spacetime.inla.call( FM=FM, DATA=DATA, SPDE=SPDE, FAMILY="gaussian" )
+
+    # extract summary statistics from a spatial (SPDE) analysis and update the output file
+    # inla.summary = spacetime.summary.inla.spde2 = ( RES, SPDE )
+
+    # inla.spde2.matern creates files to disk that are not cleaned up:
+    spdetmpfn = SPDE$f$spde2.prefix
+    fns = list.files( dirname( spdetmpfn ), all.files=TRUE, full.names=TRUE, recursive=TRUE, include.dirs=TRUE )
+    oo = grep( basename(spdetmpfn), fns )
+    if(length(oo)>0) file.remove( sort(fns[oo], decreasing=TRUE) )
+
+    rm( SPDE, DATA ); gc()
+
+    # predict upon grid
+
+    if ( method=="direct" ) {
+      # direct method ... way too slow to use for production runs
+      preds = as.data.frame( plocs )
+      preds$xmean = spacetime.invlink( RES$summary.fitted.values[ i_data, "mean"] )
+      preds$xsd   = spacetime.invlink( RES$summary.fitted.values[ i_data, "sd"] )
+      rm(RES, MESH); gc()
+    }
+
+    if (method=="fast") {
+      posterior.extract = function(s, rnm) {
+        # rnm are the rownames that will contain info about the indices ..
+        # optimally the grep search should only be done once but doing so would
+        # make it difficult to implement in a simple structure/manner ...
+        # the overhead is minimal relative to the speed of modelling and posterior sampling
+        i_intercept = grep("intercept", rnm, fixed=TRUE ) # matching the model index "intercept" above .. etc
+        i_spatial.field = grep("spatial.field", rnm, fixed=TRUE )
+        return(  s$latent[i_intercept,1] + s$latent[ i_spatial.field,1] )
+      }
+
+      # note: plocs seems to be treated as token locations and really its range and dims controls output
+      pG = inla.mesh.projector( MESH, loc=as.matrix( plocs ) )
+      posterior.samples = inla.posterior.sample(n=nsamples, RES)
+      rm(RES, MESH); gc()
+
+      rnm = rownames(posterior.samples[[1]]$latent )
+      posterior = sapply( posterior.samples, posterior.extract, rnm=rnm )
+      posterior = spacetime.invlink( posterior )   # return to original scale
+      rm(posterior.samples); gc()
+
+          # robustify the predictions by trimming extreme values .. will have minimal effect upon mean
+          # but variance estimates should be useful/more stable as the tails are sometimes quite long
+          for (ii in 1:nrow(posterior )) {
+            qnt = quantile( posterior[ii,], probs=c(0.025, 0.975), na.rm=TRUE )
+            toolow = which( posterior[ii,] < qnt[1] )
+            toohigh = which (posterior[ii,] > qnt[2] )
+            if (length( toolow) > 0 ) posterior[ii,toolow] = qnt[1]
+            if (length( toohigh) > 0 ) posterior[ii,toohigh] = qnt[2]
+          }
+
+      # posterior projection is imperfect right now .. not matching the actual requested locations
+      preds = data.frame( plon=pG$loc[,1], plat = pG$loc[,2])
+      preds$xmean = c( inla.mesh.project( pG, field=apply( posterior, 1, mean, na.rm=TRUE )  ))
+      preds$xsd   = c( inla.mesh.project( pG, field=apply( posterior, 1, sd, na.rm=TRUE )  ))
+      rm (pG)
+    }
+
+    if (0) {
+      require(lattice)
+      levelplot( log( xmean)  ~ plon+plat, preds, aspect="iso",
+                labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
+      levelplot( log (xsd )  ~ plon+plat, preds, aspect="iso",
+                labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
+    }
+    return(preds$xmean) 
+  }
+
+}
+   
---
+++ R/spacetime.interpolate.xyt.r
@@ -0,0 +1,226 @@
+
+
+spacetime.interpolate.xyt = function( ip, p ) {
+  #\\ harmonic in time method
+
+  if (exists( "libs", p)) RLibrary( p$libs )
+  if (is.null(ip)) if( exists( "nruns", p ) ) ip = 1:p$nruns
+   # load hdf5 data objects pointers
+  p = spacetime.db( p=p, DS="filenames" )
+
+  #---------------------
+  # data for modelling
+  # dependent vars # already link-transformed in spacetime.db("dependent")
+  Y = h5file(p$ptr$Y)["Y"]
+  hasdata = 1:length(Y)
+  bad = which( !is.finite( Y[]))
+  if (length(bad)> 0 ) hasdata[bad] = NA
+
+  # data locations
+  Yloc = h5file(p$ptr$Yloc)["Yloc"]
+  bad = which( !is.finite( rowSums(Yloc[])))
+  if (length(bad)> 0 ) hasdata[bad] = NA
+
+  hasdata = na.omit(hasdata)
+
+  #---------------------
+  # prediction locations and covariates
+  Ploc = h5file(p$ptr$Ploc)["Ploc"]  # prediction locations
+  phasdata = 1:nrow( Ploc ) # index of locs with no covariate data
+  pbad = which( !is.finite( rowSums(Ploc[])))
+  if (length(pbad)> 0 ) phasdata[ pbad ] = NA
+
+  rcP = data.frame( cbind( 
+    Prow = (Ploc[,1]-p$plons[1])/p$pres + 1,  
+    Pcol = (Ploc[,2]-p$plats[1])/p$pres + 1) )
+  # rcP$i =1:nrow(rcP) # row index
+  rcP$rc = paste( rcP$Prow, rcP$Pcol, sep="~")
+  rcP$Prow = rcP$Pcol = NULL
+  gc()
+
+  #-----------------
+  # row, col indices
+  Sloc = h5file(p$ptr$Sloc)["Sloc"]  # statistical output locations
+  rcS = data.frame( cbind( 
+    Srow = (Sloc[,1]-p$plons[1])/p$pres + 1,  
+    Scol = (Sloc[,2]-p$plats[1])/p$pres + 1))
+
+  # main loop over each output location in S (stats output locations)
+  for ( iip in ip ) {
+    dd = p$runs[ iip, "jj" ]
+    focal = t(Sloc[dd,])
+
+    S = h5file(p$ptr$S)["S"]  # statistical outputs
+
+    if ( is.nan( S[dd,1] ) ) next()
+    if ( !is.na( S[dd,1] ) ) next()
+
+    S[dd,1] = NaN   # this is a flag such that if a run fails (e.g. in mesh generation), it does not get revisited
+    # .. it gets over-written below if successful
+    # choose a distance <= p$dist.max where n is within range of reasonable limits to permit a numerical solution
+    # slow ... need to find a faster solution
+    ppp = NULL
+    ppp = try( point.in.block( focal[1,c(1,2)], Yloc[hasdata,], dist.max=p$dist.max, n.min=p$n.min, n.max=p$n.max,
+      upsampling=p$upsampling, downsampling=p$downsampling, resize=TRUE ) )
+    if( is.null(ppp)) next()
+    if (class( ppp ) %in% "try-error" ) next()
+    dist.cur = ppp$dist.to.nmax
+
+    j = hasdata[ppp$indices]
+    ndata = length(j) # number of data locations
+    if (ndata < p$n.min) next()
+
+    obs_ydata = list()
+    obs_ydata[[ p$variables$Y ]] =  Y[j] # already link-transformed in spacetime.db("dependent")
+
+    #  NOTE:: by default, all areas chosen to predict within the window.. but if covariates are involved,
+    #  this can be done only where covariates exists .. so next step is to determine this and predict
+    #  over the correct area.
+    #  Once all predictions are complete, simple (kernal-based?) interpolation
+    #  for areas without covariates can be completed
+      windowsize.half = floor(dist.cur/p$pres)# convert distance to discretized increments of row/col indices
+      pa_offsets = -windowsize.half : windowsize.half
+      pa = expand.grid( Prow = rcS[dd,1] + pa_offsets, Pcol = rcS[dd,2] + pa_offsets ) # row,col coords
+      bad = which( (pa$Prow < 1 & pa$Prow > p$nplons) | (pa$Pcol < 1 & pa$Pcol > p$nplats) )
+      if (length(bad) > 0 ) pa = pa[-bad,]
+      if (nrow(pa)< p$n.min) next()
+      pc_rc = paste( pa$Prow, pa$Pcol, sep="~" )
+      pa$i = match( pc_rc, rcP$rc)
+      bad = which( !is.finite(pa$i))
+      if (length(bad) > 0 ) pa = pa[-bad,]
+      if (nrow(pa)< p$n.min) next()
+      pa$plon = Ploc[ pa$i, 1]
+      pa$plat = Ploc[ pa$i, 2]
+
+      if (0) {
+        plot( Yloc[ppp$indices,1]~ Yloc[ppp$indices,2], col="red", pch=".")
+        points( Yloc[j,1] ~ Yloc[j,2], col="green" )
+        points( focal[1,1] ~ focal[1,2], col="blue" )
+        points( p$plons[rcS[dd,1]] ~ p$plats[rcS[dd,2]] , col="purple", pch=25, cex=2 )
+        points( p$plons[pa$Prow] ~ p$plats[ pa$Pcol] , col="cyan", pch=".", cex=0.01 )
+        points( Ploc[pa$i,1] ~ Ploc[ pa$i, 2] , col="yellow", pch=".", cex=0.7 )
+      }
+
+
+      # prediction stack:: check for covariates
+      if ( any( grepl ("predictions.direct", p$spacetime.outputs))) {
+        pa$i = phasdata[ pa$i ]  ## flag to ignore
+        kP = na.omit(pa$i)
+        if ( length( kP) < p$n.min ) next()
+        pa = pa[ which(is.finite( pa$i)),] # reduce to data locations as stack_index points only to locs with covariates
+        preds_locs = as.matrix( pa[, c("plon", "plat")] )
+        preds_eff = list()
+        if ( exists( "COV", p$variables) ) {
+          if ( length(p$variables$COV) == 1 ) {
+            pcovars = as.data.frame(Pcov[ kP ])
+          } else {
+            pcovars = as.data.frame(Pcov[ kP,])
+          }
+          colnames( pcovars ) = p$variables$COV
+          preds_eff[["covar"]] = as.list( pcovars )
+        }
+        preds_ydata = list()
+        preds_ydata[[ p$variables$Y ]] = NA ## ie. to predict
+      }
+
+
+      # -----------
+      # predictions
+      if ( any( grepl ("predictions", p$spacetime.outputs))) {
+        # do not use ifelse below ... it alters data structure
+        print( "Prediction step ..")
+
+        task = p$spacetime.outputs[grep("predictions", p$spacetime.outputs) ][1]  ## only take the first one in case there are many
+        preds = NULL
+
+      #  preds = .... model it here
+
+        if (exists("spacetime.invlink", p)) {
+          preds$xmean =  p$spacetime.invlink( preds$xmean )
+          preds$xsd =  p$spacetime.invlink( preds$xsd )
+        }
+
+        if (is.null(preds)) {
+          if ( debugrun) cat( paste( Sys.time(), deid, "prediction failure \n" ), file=p$debug.file, append=TRUE )
+          next()
+        }
+        pa = cbind( pa, preds)
+        if (debugrun) cat( paste(  Sys.time(), deid, "predictions completed \n" ), file=p$debug.file, append=TRUE )
+        if (debugrun) {
+          x11(); levelplot( xmean ~ plon+plat, pa, aspect="iso", labels=TRUE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=TRUE) )
+          x11(); levelplot( xsd   ~ plon+plat, pa, aspect="iso", labels=TRUE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
+        }
+        good = which( is.finite( rowSums(pa) ) )
+        if (length(good) < 1) next()
+        pa = pa[good,]
+
+        # update P (predictions)
+        counts = 1 # column indices
+        means = 2
+        stdevs = 3
+        ii = pa$i
+
+        P = h5file(p$ptr$P)["P"]  # predictions
+        test = rowSums( P[ii,] )
+        u = which( is.finite( test ) )  # these have data already .. update
+        if ( length( u ) > 0 ) {
+          ui = ii[u]  # locations of P to modify
+          P[ ui, counts ] = P[ ui, counts ] + 1 # update counts
+          # update SD estimates of predictions with those from other locations via the
+          # incremental  method ("online algorithm") of mean estimation after Knuth ;
+          # see https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+          stdev_update =  P[ ui, stdevs ] + ( pa$xsd[u] -  P[ ui, stdevs ] ) / P[ui, counts]
+          # update means: inverse-variance weighting   https://en.wikipedia.org/wiki/Inverse-variance_weighting
+          means_update = ( P[ ui, means ] / P[ ui, stdevs ]^2 + pa$xmean[u] / pa$xsd[u]^2 ) / ( P[ ui, stdevs]^(-2) + pa$xsd[u]^(-2) )
+          mm = which(is.finite( means_update + stdev_update ))
+          if( length(mm)> 0) {
+            # actual updates occur after everything has been computed first
+            P[ ui[mm], stdevs ] = stdev_update[mm]
+            P[ ui[mm], means ]  = means_update[mm]
+          }
+        }
+
+        # do this as a second pass in case NA's were introduced by the update .. unlikely , but just in case
+        test = rowSums( P[ii,] )
+        f = which( !is.finite( test ) ) # first time
+        if ( length( f ) > 0 ) {
+          fi = ii[f]
+          P[ fi, counts ] = 1
+          P[ fi, means ] = pa$xmean[f]
+          P[ fi, stdevs ] = pa$xsd[f]
+        }
+      
+      rm(MESH); gc()
+    }
+
+    #########
+
+    if ( any( grepl ("statistics", p$spacetime.outputs))) {
+      print( "Saving summary statisitics" )
+      # save statistics last as this is an indicator of completion of all tasks .. restarts would be broken otherwise
+      S[dd,1] = inla.summary["spatial error", "mode"]
+      S[dd,2] = inla.summary["observation error", "mode"]
+      S[dd,3] = inla.summary["range", "mode"]
+      S[dd,4] = inla.summary["range", "sd"]
+      if ( debugrun)  {
+        print( inla.summary )
+        cat( paste( Sys.time(), deid, "statistics saved  \n" ), file=p$debug.file, append=TRUE )
+      }
+    }
+
+    if(0) {
+      pps = expand.grid( plon=p$plons, plat=p$plats)
+      # zz = which(pps$plon > -50 & pps$plon < 50 & pps$plats < 50 & pps$plats > -50 ) # & P[,2] > 0   )
+      zz = which(pps$plon > min(pa$plon) & pps$plon < max(pa$plon) & pps$plat < max(pa$plat) & pps$plat > min(pa$plat) )
+      x11(); levelplot( ( P[zz,means] ) ~ plon + plat, pps[zz,], aspect="iso", labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE) )
+    }
+
+    rm( ii, good, pa, xs, xm, mi, mf, si, sf ) ; gc()
+    if ( debugrun) cat( paste( Sys.time(), deid, "end \n" ), file=p$debug.file, append=TRUE ) 
+  }  # end for loop
+
+  return( "complete" )
+
+}
+
+
