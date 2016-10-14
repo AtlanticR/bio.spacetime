3184f7486b5f8be4ba25b00fce8deceab4bc6d34
Jae S Choi <choijaeseok@gmail.com>
2016-10-14 10:31:49 -0300

    bug fixes

--- R/spacetime_db.R
+++ R/spacetime_db.R
@@ -47,58 +47,23 @@
       p$fn$stats =  file.path( p$project.root, "spacetime", paste( "spatial", "covariance", "rdata", sep=".") )
       p$fn$covmodel =  file.path( p$project.root, "spacetime", paste( "spatial", "covariate.model", "rdata", sep=".") )
       
-      # p$ptr =list()
-      # p$ptr$Y0 =    file.path( p$tmp.datadir, "input.Y0.bigmemory_description" ) # raw data
-      # p$ptr$Y =     file.path( p$tmp.datadir, "input.Y.bigmemory_description" ) # residuals of covar model or raw data if none
-      # p$ptr$Ycov =  file.path( p$tmp.datadir, "input.Ycov.bigmemory_description"  )
-      # p$ptr$Yloc =  file.path( p$tmp.datadir, "input.Yloc.bigmemory_description" )
-      # p$ptr$Ytime = file.path( p$tmp.datadir, "input.Ytime.bigmemory_description" )
-      # p$ptr$P0 =    file.path( p$tmp.datadir, "predictions0.bigmemory_description" ) # offsets from covar model
-      # p$ptr$P =     file.path( p$tmp.datadir, "predictions.bigmemory_description" )
-      # p$ptr$Psd =   file.path( p$tmp.datadir, "predictions_sd.bigmemory_description" )
-      # p$ptr$Pn =    file.path( p$tmp.datadir, "predictions_n.bigmemory_description" )
-      # p$ptr$Pcov =  file.path( p$tmp.datadir, "predictions_cov.bigmemory_description" )
-      # p$ptr$Ploc =  file.path( p$tmp.datadir, "predictions_loc.bigmemory_description" )
-      # p$ptr$Ptime = file.path( p$tmp.datadir, "predictions_time.bigmemory_description" )
-      # p$ptr$S =     file.path( p$tmp.datadir, "statistics.bigmemory_description" )
-      # p$ptr$Sloc =  file.path( p$tmp.datadir, "statistics_loc.bigmemory_description" )
-      # p$ptr$Stime = file.path( p$tmp.datadir, "statistics_time.bigmemory_description" )
+      p$ff =list()
+      p$ff$Y0 =    file.path( p$tmp.datadir, "input.Y0.ff" ) # raw data
+      p$ff$Y =     file.path( p$tmp.datadir, "input.Y.ff" ) # residuals of covar model or raw data if none
+      p$ff$Ycov =  file.path( p$tmp.datadir, "input.Ycov.ff"  )
+      p$ff$Yloc =  file.path( p$tmp.datadir, "input.Yloc.ff" )
+      p$ff$Ytime = file.path( p$tmp.datadir, "input.Ytime.ff" )
+      p$ff$P0 =    file.path( p$tmp.datadir, "predictions0.ff" ) # offsets from covar model
+      p$ff$P =     file.path( p$tmp.datadir, "predictions.ff" )
+      p$ff$Psd =   file.path( p$tmp.datadir, "predictions_sd.ff" )
+      p$ff$Pn =    file.path( p$tmp.datadir, "predictions_n.ff" )
+      p$ff$Pcov =  file.path( p$tmp.datadir, "predictions_cov.ff" )
+      p$ff$Ploc =  file.path( p$tmp.datadir, "predictions_loc.ff" )
+      p$ff$Ptime = file.path( p$tmp.datadir, "predictions_time.ff" )
+      p$ff$S =     file.path( p$tmp.datadir, "statistics.ff" )
+      p$ff$Sloc =  file.path( p$tmp.datadir, "statistics_loc.ff" )
+      p$ff$Stime = file.path( p$tmp.datadir, "statistics_time.ff" )
 
-
-      p$ptr_data =list()
-      p$ptr_data$Y0 =    gsub( "_description", "",  p$ptr$Y0 ) 
-      p$ptr_data$Y =     gsub( "_description", "",  p$ptr$Y ) 
-      p$ptr_data$Ycov =  gsub( "_description", "",  p$ptr$Ycov ) 
-      p$ptr_data$Yloc =  gsub( "_description", "",  p$ptr$Yloc )
-      p$ptr_data$Ytime = gsub( "_description", "",  p$ptr$Ytime )
-      p$ptr_data$P0 =    gsub( "_description", "",  p$ptr$P0 )
-      p$ptr_data$P =     gsub( "_description", "",  p$ptr$P )
-      p$ptr_data$Psd =   gsub( "_description", "",  p$ptr$Psd )
-      p$ptr_data$Pn =    gsub( "_description", "",  p$ptr$Pn )
-      p$ptr_data$Pcov =  gsub( "_description", "",  p$ptr$Pcov )
-      p$ptr_data$Ploc =  gsub( "_description", "",  p$ptr$Ploc )
-      p$ptr_data$Ptime = gsub( "_description", "",  p$ptr$Ptime )
-      p$ptr_data$S =     gsub( "_description", "",  p$ptr$S )
-      p$ptr_data$Sloc =  gsub( "_description", "",  p$ptr$Sloc )
-      p$ptr_data$Stime = gsub( "_description", "",  p$ptr$Stime )
-
-      p$bm =list()
-      p$bm$Y0 =    basename( p$ptr_data$Y0 ) 
-      p$bm$Y =     basename( p$ptr_data$Y ) 
-      p$bm$Ycov =  basename( p$ptr_data$Ycov ) 
-      p$bm$Yloc =  basename( p$ptr_data$Yloc )
-      p$bm$Ytime = basename( p$ptr_data$Ytime )
-      p$bm$P0 =    basename( p$ptr_data$P0 )
-      p$bm$P =     basename( p$ptr_data$P )
-      p$bm$Psd =   basename( p$ptr_data$Psd )
-      p$bm$Pn =    basename( p$ptr_data$Pn )
-      p$bm$Pcov =  basename( p$ptr_data$Pcov )
-      p$bm$Ploc =  basename( p$ptr_data$Ploc )
-      p$bm$Ptime = basename( p$ptr_data$Ptime )
-      p$bm$S =     basename( p$ptr_data$S )
-      p$bm$Sloc =  basename( p$ptr_data$Sloc )
-      p$bm$Stime = basename( p$ptr_data$Stime )
-      
       return(p)
     }
 
@@ -117,8 +82,8 @@
     # --------------------------
     if (DS %in% "cleanup" ) {
       p = spacetime_db( p=p, DS="filenames" )
-      for (fn in p$ptr ) if (file.exists(fn)) file.remove(fn)
-      for (fn in p$ptr_data ) if (file.exists(fn)) file.remove(fn)
+      # for (fn in p$ptr ) if (file.exists(fn)) file.remove(fn)
+      for (fn in p$ff ) if (file.exists(fn)) file.remove(fn)
       return( "done" )
     }
 
@@ -133,29 +98,29 @@
 
       # dependent variable
       Y_ = B[, p$variables$Y ]
-      p$ptr$Y0 = ff( Y_, dim=dim(Y_), file=p$ptr_data$Y0, overwrite=TRUE )
+      p$ptr$Y0 = ff( Y_, dim=dim(Y_), file=p$ff$Y0, overwrite=TRUE )
 
       if (exists( "spacetime_covariate_spacetime_engine_modelformula", p)) {
         Yresid_ = residuals( spacetime_db( p=p, DS="model.covariates") )
       } else {
         Yresid_ = Y_  # if no residual model simply use the raw data
       }
-      p$ptr$Y = ff( Yresid_, dim=dim(Yresid_), file=p$ptr_data$Y, overwrite=TRUE )
+      p$ptr$Y = ff( Yresid_, dim=dim(Yresid_), file=p$ff$Y, overwrite=TRUE )
 
      # data coordinates
       Yloc_ = as.matrix( B[, p$variables$LOCS ])
-      p$ptr$Yloc = ff( Yloc_, dim=dim(Yloc_), file=p$ptr_data$Yloc, overwrite=TRUE )
+      p$ptr$Yloc = ff( Yloc_, dim=dim(Yloc_), file=p$ff$Yloc, overwrite=TRUE )
 
       # independent variables/ covariate
       if (exists("COV", p$variables)) {
         Ycov_ = as.matrix( B[ , p$variables$COV ] )
-        p$ptr$Ycov = ff( Ycov_, dim=dim(Ycov_), file=p$ptr_data$Ycov, overwrite=TRUE )
+        p$ptr$Ycov = ff( Ycov_, dim=dim(Ycov_), file=p$ff$Ycov, overwrite=TRUE )
       }
 
       # data times
       if ( exists("TIME", p$variables) ) {
         Ytime_ = as.matrix( B[, p$variables$TIME ] )
-        p$ptr$Ytime = ff( Ytime_, dim=dim(Ytime_), file=p$ptr_data$Ytime, overwrite=TRUE )
+        p$ptr$Ytime = ff( Ytime_, dim=dim(Ytime_), file=p$ff$Ytime, overwrite=TRUE )
       }
 
       return( p ) #return pointers to data
@@ -172,24 +137,24 @@
         } else {
           Pcov_ = as.matrix( B$COV[,p$variables$COV ] ) 
         }
-        p$ptr$Pcov = ff( Pcov_, dim=dim(Pcov_), file=p$ptr_data$Pcov, overwrite=TRUE )
+        p$ptr$Pcov = ff( Pcov_, dim=dim(Pcov_), file=p$ff$Pcov, overwrite=TRUE )
       }
       
       # prediction times
       if (exists("TIME", p$variables)) {
         Ptime_ = as.matrix( B$TIME )
-        p$ptr$Ptime = ff( Ptime_, dim=dim(Ptime_), file=p$ptr_data$Ptime, overwrite=TRUE )
+        p$ptr$Ptime = ff( Ptime_, dim=dim(Ptime_), file=p$ff$Ptime, overwrite=TRUE )
       }
       
       # predictions and associated stats
       P_ = matrix( NaN, nrow=nrow(B$LOCS), ncol=p$nw*p$ny )
-      p$ptr$P = ff( P_, dim=dim(P_), file=p$ptr_data$P, overwrite=TRUE )
-      p$ptr$Pn = ff( P_, dim=dim(P_), file=p$ptr_data$Pn, overwrite=TRUE )
-      p$ptr$Psd = ff( P_, dim=dim(P_), file=p$ptr_data$Psd, overwrite=TRUE )
+      p$ptr$P = ff( P_, dim=dim(P_), file=p$ff$P, overwrite=TRUE )
+      p$ptr$Pn = ff( P_, dim=dim(P_), file=p$ff$Pn, overwrite=TRUE )
+      p$ptr$Psd = ff( P_, dim=dim(P_), file=p$ff$Psd, overwrite=TRUE )
 
       # prediction coordinates
       Ploc_ = as.matrix( B$LOCS )
-      p$ptr$Ploc = ff( Ploc_, dim=dim(Ploc_), file=p$ptr_data$Ploc, overwrite=TRUE )
+      p$ptr$Ploc = ff( Ploc_, dim=dim(Ploc_), file=p$ff$Ploc, overwrite=TRUE )
 
       rm(P_);gc()
 
@@ -201,12 +166,12 @@
         names(covars) = p$variables$COV
         Pcov = predict( covmodel, newdata=covars, type="response", se.fit=T ) 
         rm (covmodel, covars);gc()
-        p$ptr$P0_ = ff( Pcov$fit, dim=dim(Pcov$fit), file=p$ptr_data$P0, overwrite=TRUE )
-        p$ptr$P0sd_ = ff( Pcov$se.fit, dim=dim(Pcov$se.fit), file=p$ptr_data$P0sd, overwrite=TRUE )
+        p$ptr$P0_ = ff( Pcov$fit, dim=dim(Pcov$fit), file=p$ff$P0, overwrite=TRUE )
+        p$ptr$P0sd_ = ff( Pcov$se.fit, dim=dim(Pcov$se.fit), file=p$ff$P0sd, overwrite=TRUE )
       } else {
         P0_ = matrix( 0, nrow=nrow(B$LOCS), ncol=p$nw*p$ny )
-        p$ptr$P0 = ff( P0_, dim=dim(P0_), file=p$ptr_data$P0, overwrite=TRUE )
-        p$ptr$P0sd = ff( P0sd_, dim=dim(P0sd_), file=p$ptr_data$P0sd, overwrite=TRUE )
+        p$ptr$P0 = ff( P0_, dim=dim(P0_), file=p$ff$P0, overwrite=TRUE )
+        p$ptr$P0sd = ff( P0sd_, dim=dim(P0sd_), file=p$ff$P0sd, overwrite=TRUE )
       }
       
       return( p )
@@ -269,10 +234,10 @@
         # statistics storage matrix ( aggregation window, coords ) .. no inputs required
         # statistics coordinates
         Sloc_ = as.matrix( expand.grid( p$sbbox$plons, p$sbbox$plats ))
-        p$ptr$Sloc = ff( Sloc_, dim=dim(Sloc_), filename=p$ptr_data$Sloc, overwrite=TRUE )
+        p$ptr$Sloc = ff( Sloc_, dim=dim(Sloc_), filename=p$ff$Sloc, overwrite=TRUE )
 
         S_ = matrix( NaN, nrow=nrow(Sloc_), ncol=length( p$statsvars ) ) # NA forces into logical
-        p$ptr$S = ff( S_, dim=dim(S_), file=p$ptr_data$S, overwrite=TRUE )
+        p$ptr$S = ff( S_, dim=dim(S_), file=p$ff$S, overwrite=TRUE )
         return( p )
       }
 
