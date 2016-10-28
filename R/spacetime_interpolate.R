5bef7e533f6514abf4454d9908a57eccb8c67edd
Jae S Choi <choijaeseok@gmail.com>
2016-10-28 13:19:59 -0300

    typo

--- R/spacetime_interpolate.R
+++ R/spacetime_interpolate.R
@@ -38,7 +38,7 @@ spacetime_interpolate = function( ip=NULL, p ) {
     Y = Y[]
   }
 
-  if (p$spacetime_engine="habitat") {
+  if (p$spacetime_engine=="habitat") {
     Ylogit = spacetime_attach( p$storage.backend, p$ptr$Ylogit )
     Plogit = spacetime_attach( p$storage.backend, p$ptr$Plogit )
     Plogitsd = spacetime_attach( p$storage.backend, p$ptr$Plogitsd )
@@ -364,7 +364,7 @@ spacetime_interpolate = function( ip=NULL, p ) {
         Pn [vi] = 1
         P  [vi] = res$predictions$mean[v]
         Psd[vi] = res$predictions$sd[v]
-        if (p$spacetime_engine="habitat") {
+        if (p$spacetime_engine=="habitat") {
           Plogit  [vi] = res$predictions$logitmean[v]
           Plogitsd[vi] = res$predictions$logitsd[v]
         }
@@ -393,7 +393,7 @@ spacetime_interpolate = function( ip=NULL, p ) {
         } 
         stdev_update = NULL
         means_update = NULL
-        if (p$spacetime_engine="habitat") {
+        if (p$spacetime_engine=="habitat") {
           logit_stdev_update =  Plogitsd[ui,] + ( res$predictions$logitsd[u] -  Plogitsd[ui,] ) / Pn[ui]
           logit_means_update = ( Plogit[ui,] / Plogitsd[ui,]^2 + res$predictions$logitmean[u] / res$predictions$logitsd[u]^2 ) / ( Plogitsd[ui,]^(-2) + res$predictions$logitsd[u]^(-2) )
           mm = which(is.finite( logit_means_update + logit_stdev_update ))
@@ -415,7 +415,7 @@ spacetime_interpolate = function( ip=NULL, p ) {
         Pn [vi,] = 1
         P  [vi,] = res$predictions$mean[v]
         Psd[vi,] = res$predictions$sd[v]
-        if (p$spacetime_engine="habitat") {
+        if (p$spacetime_engine=="habitat") {
           Plogit  [vi,] = res$predictions$logitmean[v]
           Plogitsd[vi,] = res$predictions$logitsd[v]
         }
