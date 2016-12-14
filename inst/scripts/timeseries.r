65764bb0d131e335b1b57624c84353de395bffca
Jae S Choi <choijaeseok@gmail.com>
2016-12-14 12:51:12 -0400

    clean up name transition

--- inst/scripts/timeseries.r
+++ inst/scripts/timeseries.r
@@ -25,8 +25,8 @@
 
   x = sunspot.year
   x = sunspots
-  o = conker_timeseries( x=x, freq=frequency(x) ) # default = spec.pgram
-  o = conker_timeseries( x=x, method="fft", freq=frequency(x) )
+  o = lstfilter_timeseries( x=x, freq=frequency(x) ) # default = spec.pgram
+  o = lstfilter_timeseries( x=x, method="fft", freq=frequency(x) )
 
 
 
