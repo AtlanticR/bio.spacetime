1ba228dd36cf61e2a8e74e51698bb0934c667e26
Jae S Choi <choijaeseok@gmail.com>
2017-01-02 18:59:56 -0400

    cleanup

--- R/spatial_parameters_to_raster.R
+++ R/spatial_parameters_to_raster.R
@@ -3,49 +3,31 @@ spatial_parameters_to_raster = function( params ) {
   #\\ Take a spatial parameter list wirh corners and resolution and CRS
   #\\ and convert to a raster template
   #\\ bio uses left edge as coordinates, raster uses center
+  #\\ resolution will not be exact due to round off error
 
-  # if (edge.reference) {
-  #   params$corners$plon = params$corners$plon + c(-0.5, 0.5)*params$pres
-  #   params$corners$plat = params$corners$plat + c(-0.5, 0.5)*params$pres
-
-  #   if (params$spatial.domain=="canada.east") {
-  #     params$corners$plon = params$corners$plon + c(0, -0.5)*params$pres
-  #     params$corners$plat = params$corners$plat + c(0, -0.5)*params$pres
-  #   }
-  #   if (params$spatial.domain=="canada.east.highres") {
-  #     params$corners$plon = params$corners$plon + c(-1/2, -1/2)*params$pres
-  #     params$corners$plat = params$corners$plat + c(-1/2, -1/2)*params$pres
-  #   }
-  #   if (params$spatial.domain=="canada.east.superhighres") {
-  #     params$corners$plon = params$corners$plon + c(-1/2, -1/2)*params$pres
-  #     params$corners$plat = params$corners$plat + c(-1/2, +1/2)*params$pres
-  #   }
-     
-  # }
-#  if  ( params$spatial.domain=="canada.east.superhighres" ) {
-    params$corners$plon = params$corners$plon + c(-1/2, 0)*params$pres
-    params$corners$plat = params$corners$plat + c(-1/2, +1/2)*params$pres # 
- # }
+  params$corners$plon = params$corners$plon + c(-1/2, -1/2)*params$pres
+  params$corners$plat = params$corners$plat + c(-1/2, -1/2)*params$pres # 
 
   ras = raster::raster(
     ncols=params$nplons,
     nrows=params$nplats,
-    res=params$pres ,
+    #res=params$pres ,
     xmn= params$corners$plon[1], # rasters are center referenced
     xmx= params$corners$plon[2],
     ymn= params$corners$plat[1],
     ymx= params$corners$plat[2],
     # ext=extent ( rbind( params$corners$plon, params$corners$plat ) ),
-    crs=params$internal.crs )
+    crs=params$internal.crs 
+  )
 
 
- # if ( (dim(ras)[1] != params$nplats) | (dim(ras)[2] != params$nplons) ) {
+  if ( (dim(ras)[1] != params$nplats) | (dim(ras)[2] != params$nplons) ) {
     print( dim(ras))
     print ( paste( params$nplats, params$nplons) )
     print( ras)
     print( params$corners )
- #   stop( "Parameters to raster is dimensionally not correct")
- # } 
+    stop( "Parameters to raster is dimensionally not correct")
+  } 
 
   return(ras)
 
@@ -53,7 +35,6 @@ spatial_parameters_to_raster = function( params ) {
     bioLibrary("bio.spacetime")
     require("rgdal")
     require("raster")
-
 
     params = spatial_parameters( type="canada.east.superhighres" )
     u = spatial_parameters_to_raster(params)
