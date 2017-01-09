0731bdfc2174a2ba60049bd22c034c4df21448b9
Jae S Choi <choijaeseok@gmail.com>
2017-01-08 23:38:35 -0400

    fix fast method

--- R/spatial_warp.R
+++ R/spatial_warp.R
@@ -1,5 +1,5 @@
 
-spatial_warp = function( Z0, L0, L1, p0, p1, method="fast" ) {
+spatial_warp = function( Z0, L0, L1, p0, p1, method="fast", L0i=NULL, L1i=NULL ) {
   #\\ regrid/reproject from p0 to p1 ; rgdal calls this "warping" ;)
 message("deprecated")
 
@@ -16,37 +16,28 @@ message("deprecated")
   # ----------
 
   if (method=="fast") {
-      # extract coords, convert to new coords, interpolate where required an
-      M = matrix( NA, nrow=p0$nplons, ncol=p0$nplats) # matrix respresentation of the data in new coord system
-      L0i = as.matrix( round( cbind(
-          ( L0$plon-p0$plons[1])/p0$pres + 1, (L0$plat-p0$plats[1])/p0$pres + 1 ) ) )
 
-      L1i = as.matrix( round( cbind(
-        ( L1$plon-p1$plons[1])/p1$pres + 1, (L1$plat-p1$plats[1])/p1$pres + 1
-      ) ) )
+    L0_mat = matrix(NA, nrow=p0$nplons, ncol=p0$nplats )
+    if (is.null(L0i)) L0i = array_map( "xy->2", L0[, c("plon", "plat")],
+      corner=c(p0$plons[1], p0$plats[1]), res=c(p0$pres, p0$pres) )
+    L0_mat[L0i] = Z0
+    L0_grid = list(x=p0$plons, y=p0$plats, z=L0_mat)
 
-      L1 = planar2lonlat( L1, proj.type=p1$internal.crs )
-      L1$plon_1 = L1$plon # store original coords
-      L1$plat_1 = L1$plat
-      L1 = lonlat2planar( L1, proj.type=p0$internal.crs )
-      p1_wgts = fields::setup.image.smooth(
-        nrow=p1$nplons, ncol=p1$nplats, dx=p1$pres, dy=p1$pres,
-        theta=p1$pres, xwidth=4*p1$pres, ywidth=4*p1$pres )
+    L1_interp = fields::interp.surface( L0_grid, loc=L1[, c("plon", "plat")] ) #linear interpolation
+    ii = which( !is.finite( L1_interp ) )
+    if ( length( ii) > 0 ) {
+      L1_mat = matrix(NA, nrow=p1$nplons, ncol=p1$nplats )
+      if (is.null(L1i)) L1i = array_map( "xy->2", L1[, c("plon", "plat")],
+        corner=c(p1$plons[1], p1$plats[1]), res=c(p1$pres, p1$pres) )
+      L1_mat[L1i] = L1_interp
+      if (!exists("wght", p1)) p1$wght = fields::setup.image.smooth( nrow=p1$nplons,
+        ncol=p1$nplats, dx=p1$pres, dy=p1$pres, theta=p1$pres, xwidth=4*p1$pres, ywidth=4*p1$pres )
+      L1_sm = fields::image.smooth( L1_mat, dx=p1$pres, dy=p1$pres, wght=p1$wght )
+      L1_sm_interp = fields::interp.surface( list(x=p1$plons, y=p1$plats, z=L1_sm$z), loc=L1[, c("plon_1", "plat_1")] ) #linear interpolation from smoothed surface
+      L1_interp[ii] = L1_sm_interp[ii]
+    }
+    return( L1_interp)
 
-      M[L0i] = Z0_vn
-
-      Znew = fields::interp.surface( list(x=p0$plons, y=p0$plats, z=M), loc=L1[, c("plon", "plat")] ) #linear interpolation
-      L1_vn = c(Znew)
-      ii = which( !is.finite( L1_vn ) )
-      if ( length( ii) > 0 ) {
-        MM = matrix(NA, nrow=p1$nplons, ncol=p1$nplats )
-        MM[L1i] = L1_vn
-        Znew = fields::image.smooth( MM, dx=p1$pres, dy=p1$pres, wght=p1_wgts )
-        Zii = fields::interp.surface( list(x=p1$plons, y=p1$plats, z=Znew$z), loc=L1[, c("plon_1", "plat_1")] ) #linear interpolation
-        L1_vn[ii] = Zii[ii]
-      }
-
-      return( L1 )
   }
 
 }
